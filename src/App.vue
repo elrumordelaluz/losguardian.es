8454a50ba23f63dafdcedbec0af4e95b26c723c8
Lionel T <elrumordelaluz@hotmail.com>
2018-08-07 00:00:48 +0200

    Init :boom:

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:300");
+html {
+  height: 100%;
+  font-size: calc(1em + 1vw);
+}
+@media screen and (min-width: 50em) {
+  html {
+    font-size: 2vw;
+  }
+}
+body {
+  background-color: #5ec1c9;
+  height: 100%;
+}
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
 }
 #nav {
   padding: 30px;
